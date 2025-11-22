63e26c60bc5f27888d42b70adc806226160b57f9
arpitagwd <arpitagawade03@gmail.com>
2025-11-22 20:59:45 +0530

    Update User.js

--- backend/modelsmongodb/User.js
+++ backend/modelsmongodb/User.js
@@ -1,23 +1,4 @@
 
-// import mongoose from 'mongoose';
-// import bcrypt from 'bcryptjs';
-
-// const userSchema = new mongoose.Schema({
-//     Firstname: { type: String, required: true },
-//     Lastname: { type: String, required: true },
-//     email: { type: String, required: true, unique: true },
-//     password: { type: String, required: true },
-//     phonenum: { type: String, required: true },
-//     address: { type: String, required: true },
-// });
-
-// // export const User = mongoose.model("User", userSchema);
-
-// const User = mongoose.model("User", userSchema);
-// export default User; 
-
-
-
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
